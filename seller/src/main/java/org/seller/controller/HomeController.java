b5f54b09cf2855996dccbb7ff01219d8f8caef27
rafiduekula0009 <rafidudekula009@gmail.com>
2024-07-10 13:15:30 +0530

    communicaiton between seller and util

--- seller/src/main/java/org/seller/controller/HomeController.java
+++ seller/src/main/java/org/seller/controller/HomeController.java
@@ -5,14 +5,24 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.org.example.MyUtil;
 @Controller
 public class HomeController {
 
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
+		System.out.println(MyUtil.printName("Rafi"));
 		return new ModelAndView("home");
+	}
+	
+	@RequestMapping(value="/testUtil/{name}")
+	@ResponseBody
+	public String testUtil(HttpServletResponse response, @PathVariable("name") String name) throws IOException{
+		return MyUtil.printName(name);
 	}
 }
---
+++ util/src/main/java/com/org/example/MyUtil.java
@@ -0,0 +1,8 @@
+package com.org.example;
+
+public class MyUtil {
+
+	public static String printName(String name) {
+		return "Hi "+name+", this is util Main class!!";
+	}
+}
