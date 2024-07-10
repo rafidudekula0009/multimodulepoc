11da0ce37412f01072d39494deb53d901e7339ed
rafiduekula0009 <rafidudekula009@gmail.com>
2024-07-10 12:49:20 +0530

    seller spring-mvc module and util general module

---
+++ src/main/java/com/org/example/businessplatfrm/config/MvcConfiguration.java
@@ -0,0 +1,31 @@
+package com.org.example.businessplatfrm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages="com.org.example.businessplatfrm")
+@EnableWebMvc
+public class MvcConfiguration extends WebMvcConfigurerAdapter{
+
+	@Bean
+	public ViewResolver getViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	
+}
